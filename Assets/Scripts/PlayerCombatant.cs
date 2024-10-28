3ff81686d917767b295ebbc2e739391d0f8e4ecf
MehmetMSahin <m.sahin@se22.qmul.ac.uk>
2024-10-28 10:10:47 +0000

    Added the constructor
    Also removed the default call to randomise

--- Assets/Scripts/PlayerCombatant.cs
+++ Assets/Scripts/PlayerCombatant.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlayerCombatant : Combatant
 {
+	public PlayerCombatant(int lvl, float experiencePoints, float healthMax, float health, float attackPower, float agility) : base(lvl, healthMax, attackPower, agility)
+	{
+		this.health = health;
+		this.experiencePoints = experiencePoints;
+	}
+
 	private void Awake()
 	{
 		isAlly = true;
-		RandomiseStats();
 	}
 }
