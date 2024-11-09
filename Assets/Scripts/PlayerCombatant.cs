503f737dd313e4c7338cde6ab9160d8a302083e8
MehmetMSahin <m.sahin@se22.qmul.ac.uk>
2024-11-09 16:57:56 +0000

    Empowerment bar now updates depending on the type of damage taken

--- Assets/Scripts/PlayerCombatant.cs
+++ Assets/Scripts/PlayerCombatant.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class PlayerCombatant : Combatant
 {
-	public PlayerCombatant(int lvl, float experiencePoints, float healthMax, float health, float attackPower, float agility) : base(lvl, healthMax, attackPower, agility)
-	{
-		this.health = health;
-		this.experiencePoints = experiencePoints;
-	}
-
 	private void Awake()
 	{
 		isAlly = true;
