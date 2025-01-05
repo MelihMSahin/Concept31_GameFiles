fd889321f1bc3530572ee83c8a6bd45adc015ef1
MehmetMSahin <120316756+MelihMSahin@users.noreply.github.com>
2025-01-05 19:55:24 +0000

    MergeConflict

--- Assets/Scripts/OutOfCombatScripts/EnemyPathfinding.cs
+++ Assets/Scripts/OutOfCombatScripts/EnemyPathfinding.cs
@@ -9,7 +9,12 @@ public class EnemyPathfinding : MonoBehaviour
     public Transform player;
 
     private Vector2[] directions = { Vector2.right, Vector2.down, Vector2.left, Vector2.right, new Vector2(1,1), new Vector2(1, -1), new Vector2(-1, 1), new Vector2(-1, -1)};
+<<<<<<< Updated upstream
     [SerializeField]  private Vector2 moveDir;
+=======
+    [SerializeField] protected Vector2 moveDir;
+
+>>>>>>> Stashed changes
     public float moveSpeed;
     public float maxWait;
 
@@ -61,7 +66,58 @@ public class EnemyPathfinding : MonoBehaviour
         
     }
 
+<<<<<<< Updated upstream
     private float Normalise(float a, float b)
+=======
+    protected virtual void PlayerHit(float speed)
+	{
+        lastPlayerPosition = player.position;
+        isAtLastPlayerPosition = false;
+        rigidbody.velocity = new Vector3(Normalise(player.position.x, transform.position.x) * speed, rigidbody.velocity.y, Normalise(player.position.z, transform.position.z) * speed);
+    }
+
+    protected virtual void LastSeenPos(float speed)
+	{
+        rigidbody.velocity = new Vector3(Normalise(lastPlayerPosition.x, transform.position.x) * speed, rigidbody.velocity.y, Normalise(lastPlayerPosition.z, transform.position.z) * speed);
+        if (lastPlayerPosition.magnitude - transform.position.magnitude < 3)
+        {
+            isAtLastPlayerPosition = true;
+        }
+    }
+
+    private void Pathfinding()
+	{
+        RaycastHit hit;
+
+        float speed = Random.Range(0.5f, moveSpeed);
+        if (Physics.Raycast(transform.position, player.position - transform.position, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                //Debug.DrawRay(transform.position, transform.TransformDirection(player.position - transform.position) * hit.distance, Color.yellow);
+                lastPlayerPosition = player.position;
+                isAtLastPlayerPosition = false;
+                rigidbody.velocity = new Vector3(Normalise(player.position.x, transform.position.x) * speed, rigidbody.velocity.y, Normalise(player.position.z, transform.position.z) * speed);
+            }
+            else if (!isAtLastPlayerPosition)
+            {
+                rigidbody.velocity = new Vector3(Normalise(lastPlayerPosition.x, transform.position.x) * speed, rigidbody.velocity.y, Normalise(lastPlayerPosition.z, transform.position.z) * speed);
+                if (lastPlayerPosition.magnitude - transform.position.magnitude < 3)
+                {
+                    isAtLastPlayerPosition = true;
+                }
+            }
+            else
+            {
+                //Debug.DrawRay(transform.position, transform.TransformDirection(player.position - transform.position) * hit.distance, Color.white);
+                rigidbody.velocity = new Vector3(moveDir.x * speed, rigidbody.velocity.y, moveDir.y * speed);
+            }
+        }
+    }
+
+
+    protected float Normalise(float a, float b)
+>>>>>>> Stashed changes
 	{
         float numerator = a - b;
         float denomenator = Mathf.Abs(numerator);
